7e5b40d54b0da8817d833eb58aac34ed4262a817
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2019-08-01 18:48:25 +0530

    Main Project

---
+++ app/src/main/java/com/example/student/mywallet/Daily.java
@@ -0,0 +1,20 @@
+package com.example.student.mywallet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Daily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily);
+    }
+
+    public void addData(View view){
+        Intent intent = new Intent(Daily.this,Add.class);
+        startActivity(intent);
+    }
+}
