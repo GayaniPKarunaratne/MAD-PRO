f3e0ebe0ac5afd86f085c0721345266644bedf93
Thenukaheshith98 <Thenukaheshith@gmail.com>
2019-08-04 01:04:20 +0530

    update Thenuka five

---
+++ app/src/main/java/com/example/student/mywallet/income_category.java
@@ -0,0 +1,17 @@
+package com.example.student.mywallet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class income_category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income_category);
+
+
+    }
+}
