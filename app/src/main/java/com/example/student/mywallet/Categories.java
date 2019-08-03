e24bf9ff18a739abe34494424ab5752b5b199522
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2019-08-03 11:55:50 +0530

    Gayani seventh update

--- app/src/main/java/com/example/student/mywallet/Categories.java
+++ app/src/main/java/com/example/student/mywallet/Categories.java
@@ -1,7 +1,9 @@
 package com.example.student.mywallet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Categories extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+    }
+
+    public void addData6(View view){
+        Intent intent =new Intent(Categories.this,Add.class);
+        startActivity(intent);
+    }
+
+    public void addData7(View view){
+        Intent intent =new Intent(Categories.this,AddExpenses.class);
+        startActivity(intent);
     }
 }
