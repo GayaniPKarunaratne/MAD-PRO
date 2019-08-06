090709c67822802bfa5fd307182febf3206654e0
Thenukaheshith98 <Thenukaheshith@gmail.com>
2019-08-06 09:04:41 +0530

    Gayani 11th update

--- app/src/main/java/com/example/student/mywallet/Add.java
+++ app/src/main/java/com/example/student/mywallet/Add.java
@@ -4,13 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Add extends AppCompatActivity {
+
+    EditText cost;
+    ImageButton imaBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        cost = (EditText)findViewById(R.id.editText8);
+        imaBtn = (ImageButton)findViewById(R.id.imageButton4);
+
+        imaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!cost.getText().toString().isEmpty()){
+                    Toast.makeText(Add.this,"Thank you",Toast.LENGTH_LONG).show();
+                    Intent intent =new Intent(Add.this,Income_details.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(Add.this,"Please Input Value",Toast.LENGTH_SHORT).show();
+                    Intent intent =new Intent(Add.this,Income_details.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     public void addData5(View view){
