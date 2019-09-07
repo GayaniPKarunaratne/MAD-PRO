03a8cce62516f8800ced3c4feb7ef3607125b761
prabhashiisurindi <prabhashiisurindi@gmail.com>
2019-09-07 14:04:25 +0530

    prabhashi's new updte DB

--- app/src/main/java/Database/WalletDBhelper.java
+++ app/src/main/java/Database/WalletDBhelper.java
@@ -1,5 +1,6 @@
 package Database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -26,4 +27,24 @@ public class WalletDBhelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    //prabhashi's methods=========================================================================
+
+    public boolean addIncome(String name){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues  contentValues = new ContentValues();
+        contentValues.put(WalletUserMaster.IncomeCategory.COLUMN_NAME_INCOMENAME,name);
+        long result = db.insert(WalletUserMaster.IncomeCategory.TABLE_NAME_INCOME,null,contentValues);
+        if(result > 0){
+            return true;
+
+        }
+        else{
+            return  false;
+        }
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/student/mywallet/AddMore.java
+++ app/src/main/java/com/example/student/mywallet/AddMore.java
@@ -4,14 +4,48 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import Database.WalletDBhelper;
 
 public class AddMore extends AppCompatActivity {
+
+    WalletDBhelper db;
+    EditText txt_name;
+    TextView data;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_more);
+
+        txt_name = findViewById(R.id.editText10);
+        data = findViewById(R.id.data);
+        db = new WalletDBhelper(this);
+
     }
+
+    public  void addIncomeCat(View view){
+        name = txt_name.getText().toString();
+
+        boolean result = db.addIncome(name);
+        if(result == true){
+            Toast.makeText(getApplicationContext(),"Data Added ",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Data failed",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+
+
     public void addData1(View view){
         Intent intent = new Intent(AddMore.this,Daily.class);
         startActivity(intent);
