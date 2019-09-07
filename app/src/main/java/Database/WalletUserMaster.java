81fa1fccaa59baac6079c4ba080586602a8ac249
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2019-09-07 12:54:24 +0530

    Gayani 18th Update

---
+++ app/src/main/java/Database/WalletUserMaster.java
@@ -0,0 +1,13 @@
+package Database;
+
+import android.provider.BaseColumns;
+
+public final class WalletUserMaster {
+
+    private WalletUserMaster(){ }
+
+    protected static class Addexpences implements BaseColumns{
+        protected static final String TABLE_NAME = "addexpences";
+    }
+
+}
