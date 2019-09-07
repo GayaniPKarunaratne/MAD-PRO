21d455b0f73a0c430ad7750aca8b6585092d7194
Thenuka <Thenukaheshith@gmail.com>
2019-09-07 13:12:15 +0530

    12th update

--- app/src/main/java/com/example/student/mywallet/Acoounteditupdate.java
+++ app/src/main/java/com/example/student/mywallet/Acoounteditupdate.java
@@ -1,7 +1,9 @@
 package com.example.student.mywallet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Acoounteditupdate extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Acoounteditupdate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acoounteditupdate);
+    }
+
+    public void account(View view){
+        Intent intent = new Intent(Acoounteditupdate.this,AddExpenses.class);
+        startActivity(intent);
     }
 }
